4dbc6890e26f95afe5a54c81d63f65a09f445a1c
theioztream <daniel.calibeta@gmail.com>
2012-11-03 17:32:05 -0200

    Added some stuff for jpsupport: Now loads properly 	modified:   jpsupport/AEJPSupport.plist 	modified:   jpsupport/Tweak.mm 	deleted:    jpsupport/control 	new file:   lib/.keep

--- jpsupport/Tweak.mm
+++ jpsupport/Tweak.mm
@@ -1,16 +1,25 @@
 #include <substrate.h>
+#import <Foundation/Foundation.h>
 
-static NSArray* (*original_AFPreferencesSupportedLanguages)();
+static NSArray *(*original_AFPreferencesSupportedLanguages)();
 
-static NSArray* replaced_AFPreferencesSupportedLanguages() {
-    NSArray* orig = original_AFPreferencesSupportedLanguages();
-    NSMutableArray* repl = [NSMutableArray arrayWithArray:orig];
-    [repl addObject:@"ja-JP"];
+static NSArray *replaced_AFPreferencesSupportedLanguages() {
+    NSArray *orig = original_AFPreferencesSupportedLanguages();
+    NSMutableArray *repl = [NSMutableArray arrayWithArray:orig];
+    
+    // This check is not necessary since we only load at 5.0-5.1.
+    // But precaution anyway.
+    if (![repl containsObject:@"ja-JP"]) {
+		[repl addObject:@"ja-JP"];
+	}
 
     return repl;
 }
 
-__attribute__((constructor)) void AEJPSupportInit() {
+__attribute__((constructor))
+static void AEJPSupportInit() {
+	NSLog(@"[AssistantExtensions] JPSupport: Loading.");
+	
     // FIXME: Should I not pass NULL as first argument of MSFindSymbol?
     MSHookFunction((NSArray*(*)())MSFindSymbol(NULL, "_AFPreferencesSupportedLanguages"), replaced_AFPreferencesSupportedLanguages, &original_AFPreferencesSupportedLanguages);
 }
